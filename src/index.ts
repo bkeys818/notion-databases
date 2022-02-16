21abd81927c6724bf9b6c84f2ee491da31509a6e
Ben Keys <bkeys818@gmail.com>
2022-02-16 10:13:17 -0500

    💥 Remove type Properties and CustomPage exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 import Database from './database'
 import NotionClient from './request'
 export { Database, NotionClient }
-export type {
-    Item,
-    Page,
-    Property,
-    PropertyType,
-    Properties,
-    CustomProps,
-} from './item'
+export type { Item, Page, Property, PropertyType } from './item'
