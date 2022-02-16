532864ca905e9b6397b08306f682056179bf7902
Ben Keys <bkeys818@gmail.com>
2022-02-16 10:04:51 -0500

    💥 Make 'items' a promise

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,13 @@ export class Database<P extends CustomProps, T extends Item<P>> {
         private readonly filter?: Filter
     ) {}
 
-    readonly items: T[] = []
-
-    readonly getItemsPromise = getEntireList(
-        this.client.database.query, {
+    readonly items = getEntireList(this.client.database.query, {
         database_id: this.id,
         page_size: 100,
-        filter: this.filter
-    }).then((list) => {
-        this.items.push(
-            ...list.map(
-                item => new this.item(item as Page<P>, this.client.pages.update)
-            )
-        )
-    })
+        filter: this.filter,
+    }).then(pages => pages.map((page) => 
+        new this.item(page as Page<P>, this.client.pages.update)
+    ))
 
     newItem(properties: Partial<Properties<P>>) {
         return this.client.pages.create({
