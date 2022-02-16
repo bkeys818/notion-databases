6f19b4781f2958ba898864b14e27fe2dbd89e9b5
Ben Keys <bkeys818@gmail.com>
2022-02-16 10:12:35 -0500

    🏗️ Move database to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
-import { getEntireList } from './list'
+import Database from './database'
 import NotionClient from './request'
-export { NotionClient }
-import type { Item, CustomProps, ItemParams, Page, Properties } from './item'
-export { Item } from './item'
-export type { CustomProps, Page, Properties }
-export type { Property, PropertyType } from './item'
-import type { Filter } from 'notion-api-types/endpoints/databases/query'
-
-export class Database<P extends CustomProps, T extends Item<P>> {
-    constructor(
-        private readonly item: new (...args: ItemParams<P>) => T,
-        private readonly client: NotionClient,
-        private readonly id: string,
-        private readonly filter?: Filter
-    ) {}
-
-    readonly items = getEntireList(this.client.database.query, {
-        database_id: this.id,
-        page_size: 100,
-        filter: this.filter,
-    }).then(pages => pages.map((page) => 
-        new this.item(page as Page<P>, this.client.pages.update)
-    ))
-
-    newItem(properties: Partial<Properties<P>>) {
-        return this.client.pages.create({
-            parent: { database_id: this.id },
-            properties: properties as Properties<P>,
-        })
-    }
-}
+export { Database, NotionClient }
+export type {
+    Item,
+    Page,
+    Property,
+    PropertyType,
+    Properties,
+    CustomProps,
+} from './item'
